55aa85caab1f6584b53e2d5ba78c885628c09f11
Kristjan Lääne <xonproject@gmail.com>
2024-08-27 19:23:26 +0300

    persist data via localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BackgroundHeading from './components/BackgroundHeading';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -7,7 +7,9 @@ import Sidebar from './components/Sidebar';
 import { initialItems } from './lib/constants';
 
 function App() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState(() => {
+    return JSON.parse(localStorage.getItem('items')) || initialItems;
+  });
 
   const handleAddItem = (newItemText) => {
     const newItem = {
@@ -59,6 +61,10 @@ function App() {
 
     setItems(newItems);
   };
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
 
   return (
     <>
